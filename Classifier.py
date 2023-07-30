f4fed3a1859075a3a10c02fc859bb550533d1075
DennisKeenan <dennis.k.arkananta@gmail.com>
2023-07-30 11:13:51 +0700

    Classifier Update (Final)

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,10 @@
 from sklearn import datasets
 import pandas as pd
 import matplotlib.pyplot as mp
+import seaborn as sb
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report,confusion_matrix
 
 # Read Data
 RAW=datasets.load_digits()
@@ -18,15 +22,43 @@ Image=RAW.images
 # while(True):
 #     number=int(input("Please input the image number: "))
 #     print(Image[number])
+#     print(Image[number].ravel())
+#     print(Data[number])
 #     print(Target[number])
 #     mp.imshow(Image[number],cmap=mp.cm.gray_r)
 #     mp.axis("Off")
 #     mp.title(Target[number])
 #     mp.show()
 
-figures,axis=mp.subplots(3,10,figsize=(15,6))
-for ax,im,number in zip(axis.ravel(),Image,Target):
-    ax.axis("Off")
-    ax.imshow(im,cmap=mp.cm.gray_r)
-    ax.set_title(number)
-mp.show()
+# Images Data
+# figures,axis=mp.subplots(3,10,figsize=(15,6))
+# for ax,im,number in zip(axis.ravel(),Image,Target):
+#     ax.axis("Off")
+#     ax.imshow(im,cmap=mp.cm.gray_r)
+#     ax.set_title(number)
+# mp.show()
+
+# Training
+x_train,x_test,y_train,y_test=train_test_split(Data,Target,test_size=0.33,random_state=99,stratify=Target)
+# print(x_train.shape)
+# print(x_test.shape)
+KNN=KNeighborsClassifier(n_neighbors=3)
+KNN.fit(x_train,y_train)
+y_predict=KNN.predict(x_test)
+# print(y_test)
+# print(y_predict)
+
+# Test Report
+report=classification_report(y_test,y_predict)
+print(report)
+
+# Matrix
+matrix=confusion_matrix(y_true=y_test,y_pred=y_predict)
+# print(matrix)
+aesthetic_matrix=sb.heatmap(matrix,annot=True,cmap="nipy_spectral_r")
+aesthetic_matrix.set_title("Confusion Matrix")
+# mp.show()
+
+# Accuracy Score
+accuracy=KNN.score(x_test,y_test)
+print(accuracy)
